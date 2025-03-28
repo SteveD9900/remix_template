0da2258466073992eee3b341a06a54d5d64204dc
Steve Deng <dengzw1994@gmail.com>
2025-03-28 21:11:50 +1100

    set up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,46 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+// declare module "@remix-run/node" {
+//   interface Future {
+//     v3_singleFetch: true;
+//   }
+// }
 
 export default defineConfig({
   plugins: [
+    remix({
+      ssr: false,
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true
+      }
+    }),
+    tsconfigPaths()
   ],
   ssr: {
     noExternal: []
   },
   build: {
+    outDir: 'build',
+    emptyOutDir: true,
     commonjsOptions: {
       transformMixedEsModules: true // for Vite to handle mixed ESM + CSJ modules in the app
+    }
+  },
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './app')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern'
+      }
     }
   }
 });
